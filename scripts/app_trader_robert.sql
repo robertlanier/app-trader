fd993a286c692b16985da348ebe75509208e6c44
Robert LaNier <45923934+base2bits@users.noreply.github.com>
2021-08-19 21:00:38 -0500

    Finalizing queries - RL

--- scripts/app_trader_robert.sql
+++ scripts/app_trader_robert.sql
@@ -1,14 +1,18 @@
 SELECT AVG(CAST(review_count AS numeric))
 FROM app_store_apps;
 
+
 SELECT AVG(rating)
 FROM app_store_apps;
+
 
 SELECT AVG(review_count)
 FROM play_store_apps;
 
+
 SELECT AVG(rating)
 FROM play_store_apps;
+
 
 SELECT *
 FROM app_store_apps
@@ -17,11 +21,13 @@ WHERE price <= 1.00
 ORDER BY CAST(review_count AS int) DESC
 LIMIT 50;
 
+
 SELECT *
 FROM play_store_apps
 WHERE CAST(price AS money) <= CAST(1.00 AS money) AND rating <= 4.5 AND content_rating = 'Everyone'
 ORDER BY review_count DESC
 LIMIT 50;
+
 
 SELECT name, rating
 FROM app_store_apps
@@ -37,6 +43,14 @@ WHERE CAST(price AS money) <= CAST(1.00 AS money)
 	AND review_count >= 444153
 ORDER BY rating DESC;
 
+
+SELECT DISTINCT *
+FROM app_store_apps AS a
+FULL JOIN play_store_apps AS p
+USING (name)
+ORDER BY name;
+
+
 SELECT DISTINCT name,
 	p.price AS playstore_price,
 	a.price AS appstore_price,
@@ -51,3 +65,57 @@ FROM app_store_apps AS a
 FULL JOIN play_store_apps AS p
 USING (name)
 ORDER BY name;
+
+---------------
+-- First CTE --
+---------------
+WITH app_store AS
+(SELECT name, primary_genre AS app_store_genres, price AS app_store_price, rating AS app_store_rating
+FROM app_store_apps
+WHERE price <= 1 AND rating >= 4.5
+GROUP BY name, primary_genre, price, rating
+ORDER BY name ASC),
+---------------
+-- Second CTE --
+---------------
+play_store AS 
+(SELECT name, genres AS play_store_genres, price AS play_store_price, rating AS play_store_rating
+FROM play_store_apps
+WHERE CAST(price AS money) <= CAST(1 AS money) AND rating >= 4.5
+GROUP BY name, genres, price, rating
+ORDER BY name ASC)
+----------------
+-- Both CTEs --
+----------------
+SELECT p.name, p.play_store_genres, a.app_store_genres, p.play_store_price, a.app_store_price, p.play_store_rating, a.app_store_rating
+FROM play_store AS p
+INNER JOIN app_store AS a ON p.name = a.name
+ORDER BY p.name ASC, p.play_store_rating DESC;
+
+
+---------------
+-- First CTE --
+---------------
+WITH app_store AS 
+(SELECT name, primary_genre AS app_store_genres, price AS app_store_price, rating AS app_store_rating
+FROM app_store_apps
+WHERE price <= 1 AND rating >= 4.5
+GROUP BY name, primary_genre, price, rating
+ORDER BY name ASC),
+---------------
+-- Second CTE --
+---------------
+play_store AS 
+(SELECT name, genres AS play_store_genres, price AS play_store_price, rating AS play_store_rating
+FROM play_store_apps
+WHERE CAST(price AS money) <= CAST(1 AS money) AND rating >= 4.5
+GROUP BY name, genres, price, rating
+ORDER BY name ASC)
+----------------
+-- Both CTEs --
+----------------
+SELECT p.name, p.play_store_genres, a.app_store_genres, p.play_store_price, a.app_store_price, p.play_store_rating, ROUND(ROUND(p.play_store_rating/5,1)*5,1) AS rounded_play_store_rating , a.app_store_rating
+FROM play_store AS p
+INNER JOIN app_store AS a ON p.name = a.name
+ORDER BY p.name ASC, p.play_store_rating DESC;
+
