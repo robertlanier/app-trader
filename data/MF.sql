30af5bcf51c8b22da926cd67b9c41a2fe228915d
meferral <meferral@gmail.com>
2021-08-24 20:04:59 -0500

    top10

--- data/MF.sql
+++ data/MF.sql
@@ -300,4 +300,115 @@ INNER JOIN app_store AS a ON p.name = a.name
 ORDER BY p.name ASC, a.app_store_rating desc;
 
 --AST(REPLACE(REPLACE(p.install_count, '+', ''), ',', '') AS numeric)
+WITH app_store AS 
+(SELECT name, primary_genre AS app_store_genres, price AS app_store_price, rating AS app_store_rating, content_rating AS app_store_content_rating
+FROM app_store_apps
+WHERE price <= 1 AND rating >= 4.5
+GROUP BY name, primary_genre, price, rating, content_rating
+ORDER BY name ASC),
 
+play_store AS 
+(SELECT name, genres AS play_store_genres, price AS play_store_price, rating AS play_store_rating, content_rating AS play_store_content_rating
+FROM play_store_apps
+WHERE CAST(price AS money) <= CAST(1 AS money) AND rating >= 4.5
+GROUP BY name, genres, price, rating, content_rating
+ORDER BY name ASC),
+
+both_stores AS 
+(SELECT p.name, p.play_store_genres, a.app_store_genres, p.play_store_price, a.app_store_price, p.play_store_rating, a.app_store_rating, p.play_store_content_rating, a.app_store_content_rating
+FROM play_store AS p
+INNER JOIN app_store AS a ON p.name = a.name
+ORDER BY p.name ASC, p.play_store_rating DESC),
+
+
+/*SELECT play_store_content_rating, COUNT(name)
+FROM both_stores
+GROUP BY play_store_content_rating*/
+
+/*SELECT app_store_content_rating, COUNT(name)
+FROM both_stores
+GROUP BY app_store_content_rating*/
+
+---Christians Code>>>
+
+both_stores_filtered AS (SELECT name, play_store_rating, play_store_price, app_store_price, play_store_content_rating, app_store_genres
+FROM both_stores
+WHERE app_store_content_rating = '4+'
+GROUP BY name, play_store_rating, play_store_price, app_store_price, play_store_content_rating, app_store_genres
+ORDER BY play_store_rating DESC),
+
+both_filtered_final AS (SELECT b.name, p.install_count, b.play_store_rating, b.play_store_content_rating, b.app_store_genres
+FROM both_stores_filtered AS b
+LEFT JOIN play_store_apps AS p
+ON b.name = p.name
+GROUP BY b.name, p.install_count, b.play_store_rating, b.play_store_content_rating, b.app_store_genres
+ORDER BY CAST(REPLACE(REPLACE(p.install_count, '+', ''), ',', '') AS numeric) ASC
+)
+
+SELECT name, play_store_rating, install_count, play_store_content_rating, app_store_genres
+FROM both_filtered_final
+ORDER BY CAST(REPLACE(REPLACE(install_count, '+', ''), ',', '') AS numeric) ASC
+LIMIT 25
+
+------- Dereks'sCode
+WITH app_store AS 
+(SELECT name, primary_genre AS app_store_genres, price AS app_store_price, rating AS app_store_rating, ROUND(((ROUND(ROUND(rating/5,1)*5,1))/.5),0) AS app_store_years_estimate
+FROM app_store_apps
+WHERE price <= 1 AND rating >= 4.5
+GROUP BY name, primary_genre, price, rating
+ORDER BY name ASC),
+
+play_store AS 
+(SELECT name, install_count, genres AS play_store_genres, price AS play_store_price, rating AS play_store_rating, ROUND(((ROUND(ROUND(rating/5,1)*5,1))/.5),0) AS play_store_years_estimate
+FROM play_store_apps
+WHERE CAST(price AS money) <= CAST(1 AS money) AND rating >= 4.5
+GROUP BY name, genres, price, rating, install_count
+ORDER BY name ASC),
+
+play_store_2 AS
+(SELECT *, ((play_store_years_estimate*12)*5000) AS play_store_app_lifetime_income, ((play_store_years_estimate*12)*1000) AS play_store_app_lifetime_cost
+ FROM play_store
+),
+
+app_store_2 AS
+(SELECT *, ((app_store_years_estimate*12)*5000) AS app_store_app_lifetime_income, ((app_store_years_estimate*12)*1000) AS app_store_app_lifetime_cost
+ FROM app_store
+)
+
+
+/*--removed from select
+-- p.play_store_genres, 
+-- a.app_store_genres, 
+-- ROUND(ROUND(p.play_store_rating/5,1)*5,1) AS rounded_play_store_rating, 
+-- a.app_store_rating, */
+SELECT 
+	p.name,
+	t.name,
+	p.play_store_years_estimate, 
+	a.app_store_years_estimate,
+	p.play_store_app_lifetime_income,
+	a.app_store_app_lifetime_income,
+	p.play_store_app_lifetime_cost + 10000 AS total_lifetime_cost,
+	a.app_store_app_lifetime_income + p.play_store_app_lifetime_income AS both_store_total_lifetime_income
+	
+FROM play_store_2 AS p
+INNER JOIN app_store_2 AS a 
+ON p.name = a.name
+
+JOIN top_25 as t
+ON p.name = t.name
+
+ORDER BY p.name ASC, p.play_store_rating DESC;
+
+-- WITH app_content_ratings AS (SELECT name, rating,
+-- 	SUM(CASE WHEN content_rating = '4+' THEN 1 ELSE 0 END) AS everyone,
+-- 	SUM(CASE WHEN content_rating = '9+' THEN 1 ELSE 0 END) AS everyone9,
+-- 	SUM(CASE WHEN content_rating = '12+' THEN 1 ELSE 0 END) AS teen,
+-- 	SUM(CASE WHEN content_rating = '17+' THEN 1 ELSE 0 END) as mature
+-- FROM app_store_apps
+-- GROUP BY name, rating)
+
+-- SELECT ROUND(SUM(everyone)/COUNT(name), 2) AS everyone_pct, ROUND(SUM(everyone9)/COUNT(name), 2) AS everyone9_pct,
+-- ROUND(SUM(teen)/COUNT(name), 2) AS teen_pct, ROUND(SUM(mature)/COUNT(name), 2) AS mature_pct
+-- FROM app_content_ratings
+-- WHERE rating >= 4.5;
